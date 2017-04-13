2e484fe9d832127836aca20dc1a76651535c25ff
Nathan Herald <me@nathanherald.com>
2017-04-13 23:29:17 +0200

    Fix deprecations and include latest mix hex metadata

--- mix.exs
+++ mix.exs
@@ -9,14 +9,14 @@ defmodule Base62.Mixfile do
      version: @version,
      elixir: "~> 1.0",
      name: "Base62",
-     description: description,
-     package: package,
+     description: description(),
+     package: package(),
      source_url: @github,
      docs: [extras: ["README.md"],
             main: "README",
             source_ref: "v#{@version}",
             source_url: @github],
-     deps: deps]
+     deps: deps()]
   end
 
   def application, do: []
